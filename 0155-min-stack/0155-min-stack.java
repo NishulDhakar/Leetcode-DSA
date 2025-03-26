b9089255c4b885bdd7a51af027d42581948158b6
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-03-26 17:10:56 +0530

    Time: 4 ms (97.28%), Space: 44.6 MB (91.99%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -13,17 +13,19 @@ class MinStack {
     public void push(int val) {
 
         stack.push(val);
-        if(minStack.isEmpty() || minStack.peek() > val){
+        if(minStack.isEmpty() || minStack.peek() >= val){
             minStack.push(val);
-        }else{
-         minStack.push(minStack.peek());
         }
         
     }
     
     public void pop() {
+
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
         stack.pop();
-        minStack.pop();
+        
 
     }
     
