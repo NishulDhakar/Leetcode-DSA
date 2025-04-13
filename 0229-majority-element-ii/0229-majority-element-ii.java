43527216adf80ed02822944e4eb754d663995be6
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-13 14:04:31 +0530

    Time: 10 ms (46.14%), Space: 47.9 MB (60.82%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        List<Integer> res = new ArrayList<>();
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        for(int key : nums){
+           map.put(key , map.getOrDefault(key , 0) +1);
+        }
+
+        Set<Integer> keySet = map.keySet();
+
+        for(Integer key : keySet){
+            if(map.get(key) > nums.length/3){
+
+                  res.add(key);
+
+            }
+        }
+
+        return res;
+        
+    }
+}
