903b8ad6fb82b179d806e048595b39bb19ed5687
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-13 15:30:47 +0530

    Time: 14 ms (29.58%), Space: 45.1 MB (12.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -13,18 +13,21 @@ class Solution {
         for(char ch : s.toCharArray()){
             map.put(ch , map.getOrDefault(ch, 0) +1);
         } 
-        for(char ch : t.toCharArray()){
-            if(map.get(ch) != null){
-                if(map.get(ch) == 1){
-                    map.remove(ch);
-                }else{
-                    map.put(ch , map.get(ch) -1);
-                }
-            }else{
+
+         for (char ch : t.toCharArray()) {
+            if (!map.containsKey(ch)) {
                 return false;
+            }
+            if (map.get(ch) == 1) {
+                map.remove(ch);
+            }else{
+             map.put(ch, map.get(ch) - 1);
+            }
+
+        }
+
+        return map.isEmpty();
             }
         } 
 
-        return map.isEmpty();
-    }
-}
+   
