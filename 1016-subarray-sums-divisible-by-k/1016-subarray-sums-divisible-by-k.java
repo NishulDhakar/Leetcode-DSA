9c169e6068c5e9a8702f85b46326e9b3fdf44c9a
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-21 10:20:09 +0530

    Time: 25 ms (36.5%), Space: 47.5 MB (55.93%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -51,30 +51,21 @@ class Solution {
 
     //----------------------------------\\
 
-    int res = 0, sum =0;
-
-     HashMap<Integer , Integer> map = new HashMap<>();
-
-     map.put(0 ,1);
-
-     for(int i = 0 ; i< n ; i++){
-
-        sum += nums[i];
-        int rem = sum%k;
-
-         if(rem < 0 ){
-            rem += k;
-         }
-
-         if(map.containsKey(rem)){
-         res += map.get(rem);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int rem = sum % k;
+            if (rem < 0) rem += k;
+            if(map.containsKey(rem)){
+                count += map.get(rem);
+            }
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
         }
+        return count;
 
-         map.put(rem , map.getOrDefault(rem , 0)+1);
-         
-     }
-
-     return res;
 
 }
 }
