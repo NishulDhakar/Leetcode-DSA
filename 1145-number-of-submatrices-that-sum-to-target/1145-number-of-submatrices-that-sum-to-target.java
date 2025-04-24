f1f773b837ba9ebffe84aaefa9f90812c221e553
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-24 11:47:50 +0530

    Time: 145 ms (48.72%), Space: 44.7 MB (81.44%) - LeetHub

--- 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
@@ -3,51 +3,51 @@ class Solution {
 
         int rows = matrix.length;
         int cols = matrix[0].length;
+
         int count = 0;
 
+
         for(int r = 0 ; r < rows ; r++){
-            for(int c = 1 ; c < cols ; c++){
+            for(int c = 1; c < cols; c++){
 
                 matrix[r][c] += matrix[r][c-1];
-
-               }
-        }
-
-        //apply leetcode 560 concept now
-
-        int result = 0;
-
-        for(int startCol = 0 ; startCol < cols ; startCol++){
-            for(int j = startCol ; j < cols ; j++){
-
-                HashMap <Integer , Integer> map = new HashMap<>();
-
-                map.put(0 ,1);
-
-                int sum = 0;
-
-                for(int row = 0 ; row < rows ; row++){
-
-                    if(startCol > 0){
-
-                        sum += matrix[row][j] - matrix[row][startCol -1];
-
-                    }else{
-                        sum += matrix[row][j];
-                    }
-
-                    if(map.containsKey(sum - target)){
-                        result += map.get(sum - target);
-                    }
-
-                    map.put(sum , map.getOrDefault(sum , 0) + 1);
-
-                }
- 
             }
         }
 
-        return result;
+  
+
+        for(int sc = 0 ; sc< cols ; sc++){
+            for(int j = sc ; j< cols ; j++){
+
+                HashMap<Integer , Integer> map = new HashMap<>();
+                map.put(0 ,1);
+
+                      int sum = 0;
+
+                for(int row = 0 ; row < rows ; row++){
+
+                    if(sc > 0){
+                        sum+= matrix[row][j] - matrix[row][sc -1];
+
+                    }else{
+                          sum+= matrix[row][j];
+                    }
+
+                    if(map.containsKey(sum - target)){
+                        count += map.get(sum - target);
+                    }
+
+                     map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+                   
+                }
+
+
+            }
+        }
+
+
+        return count;
         
     }
 }
