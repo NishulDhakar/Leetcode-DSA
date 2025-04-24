a6021317aa331a727b261450634dc1e6cc3feb45
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-24 07:36:40 +0530

    Time: 24 ms (51.23%), Space: 47 MB (78.56%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,71 +1,33 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
 
-        int n = nums.length;
+        int sum = 0 ; 
+        int res =0;
 
-        // int [] p  = new int[n];
+        HashMap <Integer , Integer> map = new HashMap<>();
 
-        // p[0] = nums[0];
+        map.put(0 ,1);
 
-        // for(int i = 1 ; i < n ; i++){
-        //     p[i] = p[i -1] + nums[i];
-        // }
+        for(int i = 0; i < nums.length; i++){
 
-        // int c = 0;
-
-        // for(int i = 0 ; i < n ; i++){
-        //     for(int j = i ; j < n ; j++){
-
-        //         int sum = 0;
-
-        //           if (i > 0) {
-        //               sum = p[j] - p[i - 1];
-        //           } else {
-        //              sum = p[j];
-        //           }
-
-        //           if(sum %k == 0){
-        //             c++;
-        //           }
-            
-        //     }
-        // }
-        // return c;
-
-        //----------------------------------\\
-
-    //     int res = 0;
-
-    //    for (int i = 0; i < n; i++) {
-    //        int sum = 0;
-    //      for (int j = i; j < n; j++) {
-    //         sum += nums[j];   
-
-    //          if (sum % k == 0) {
-    //         res++;
-    //          }
-    //      }
-    //   }
-
-    //   return res;
-
-    //----------------------------------\\
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
-        int sum = 0;
-        int count = 0;
-        for(int i = 0; i < n; i++){
             sum += nums[i];
+
             int rem = sum % k;
-            if (rem < 0) rem += k;
-            if(map.containsKey(rem)){
-                count += map.get(rem);
+
+            if(rem < 0){
+                rem +=k;
             }
-            map.put(rem, map.getOrDefault(rem, 0) + 1);
+
+            if(map.containsKey(rem)){
+
+                res+= map.get(rem);
+
+            }
+
+            map.put(rem, map.getOrDefault(rem , 0)+1);
         }
-        return count;
 
-
-}
+        return res;
+        
+    }
 }
