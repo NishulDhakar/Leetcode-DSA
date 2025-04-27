c8dd5540bd0d28bc2cdb04fc32ffe5ff1363c20a
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-04-27 11:23:57 +0530

    Time: 138 ms (55.38%), Space: 45.5 MB (10.77%) - LeetHub

--- 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/1145-number-of-submatrices-that-sum-to-target.java
@@ -3,51 +3,44 @@ class Solution {
 
         int rows = matrix.length;
         int cols = matrix[0].length;
+        int res =0;
 
-        int count = 0;
 
-
-        for(int r = 0 ; r < rows ; r++){
+        for(int r = 0; r < rows ; r++ ){
             for(int c = 1; c < cols; c++){
-
-                matrix[r][c] += matrix[r][c-1];
+                matrix[r][c] += matrix[r][c -1];
             }
         }
 
-  
+        for(int sc = 0 ; sc< cols; sc++){
+            for(int j =sc ; j< cols; j++){
 
-        for(int sc = 0 ; sc< cols ; sc++){
-            for(int j = sc ; j< cols ; j++){
 
                 HashMap<Integer , Integer> map = new HashMap<>();
                 map.put(0 ,1);
-
-                      int sum = 0;
+                int sum =0;
 
                 for(int row = 0 ; row < rows ; row++){
 
+                    int cs = matrix[row][j];
+
                     if(sc > 0){
-                        sum+= matrix[row][j] - matrix[row][sc -1];
-
-                    }else{
-                          sum+= matrix[row][j];
+                        cs = matrix[row][j] - matrix[row][sc -1];
                     }
 
-                    if(map.containsKey(sum - target)){
-                        count += map.get(sum - target);
+                    sum += cs;
+
+                    if(map.containsKey(sum -target)){
+
+                       res += map.get(sum - target);
+
                     }
+                    map.put(sum ,map.getOrDefault(sum , 0)+1);
 
-                     map.put(sum, map.getOrDefault(sum, 0) + 1);
-
-                   
                 }
-
-
             }
         }
 
-
-        return count;
-        
+        return res;
     }
 }
