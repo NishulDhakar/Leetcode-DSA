7a8e54446dba588808919001f2782bdba50c3f63
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-05-03 11:34:13 +0530

    Time: 27 ms (36.23%), Space: 46.4 MB (21.89%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.java
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -2,46 +2,65 @@ class Solution {
     public String pushDominoes(String dominoes) {
 
         int n = dominoes.length();
-        int[] forces = new int[n];
 
-        // Left to right pass (for 'R')
-        int force = 0;
-        for (int i = 0; i < n; i++) {
-            if (dominoes.charAt(i) == 'R') {
-                force = n;
-            } else if (dominoes.charAt(i) == 'L') {
-                force = 0;
-            } else {
-                force = Math.max(force - 1, 0);
-            }
-            forces[i] += force;
-        }
+       int[] leftClosestR = new int[n];
+       int[] rightClosestL = new int[n];
 
-        // Right to left pass (for 'L')
-        force = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (dominoes.charAt(i) == 'L') {
-                force = n;
-            } else if (dominoes.charAt(i) == 'R') {
-                force = 0;
-            } else {
-                force = Math.max(force - 1, 0);
-            }
-            forces[i] -= force;
-        }
+        //left to right find leftClosestR
 
-        // Construct the result
-        StringBuilder result = new StringBuilder();
-        for (int f : forces) {
-            if (f > 0) {
-                result.append('R');
-            } else if (f < 0) {
-                result.append('L');
-            } else {
-                result.append('.');
+        for(int i = 0; i< n ;i++){
+            if(dominoes.charAt(i) == 'R'){
+                leftClosestR[i] = i;
+            }else if(dominoes.charAt(i) == '.'){
+                leftClosestR[i] = i > 0 ? leftClosestR[i -1] :  -1;
+            }else{
+                 leftClosestR[i] = -1  ;
             }
         }
 
-        return result.toString();
+        //right to left find rightClosestL
+
+        for(int i = n-1; i >= 0;i--){
+            if(dominoes.charAt(i) == 'L'){
+                rightClosestL[i] = i;
+            }else if(dominoes.charAt(i) == '.'){
+                rightClosestL[i] = i < n-1  ? rightClosestL[i +  1] :  -1;
+            }else{
+                 rightClosestL[i] = -1;
+            }
+        }
+
+        char[] res = new char[n];
+
+        for(int i = 0; i< n ;i++){
+
+            if (dominoes.charAt(i) != '.') {
+                res[i] = dominoes.charAt(i);
+                continue;
+            }
+
+             int distLeftR = (leftClosestR[i] == -1) ? 
+             Integer.MAX_VALUE : Math.abs(i - leftClosestR[i]);
+
+             int distRightL = (rightClosestL[i] == -1) ?
+             Integer.MAX_VALUE : Math.abs(i - rightClosestL[i]);
+
+            if(distRightL == distLeftR || leftClosestR[i] == rightClosestL[i]){
+
+                res[i] = '.';
+
+            }else if(leftClosestR[i] == -1){
+                res[i] = 'L';
+            }else if(rightClosestL[i] == -1){
+                res[i] = 'R';
+            }else{
+
+                res[i] = distRightL < distLeftR ? 'L' : 'R';
+
+            }
+           
+        }
+
+         return new String(res);
     }
 }
