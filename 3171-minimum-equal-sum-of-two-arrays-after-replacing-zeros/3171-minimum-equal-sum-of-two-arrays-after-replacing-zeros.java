66d68e33cd77376b6f4a5fd9cceecdb8dd4b63d4
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-05-10 23:36:03 +0530

    Time: 3 ms (88.73%), Space: 60.6 MB (71.83%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,29 +1,39 @@
 class Solution {
     public long minSum(int[] nums1, int[] nums2) {
 
-        long sum1 = 0, sum2 = 0;
-        long zero1 = 0, zero2 = 0;
+        long sum1 = 0;
+        long sum2 = 0;
 
-        for (int i : nums1) {
-            sum1 += i;
-            if (i == 0) {
-                sum1++;
-                zero1++;
-            }
-        }
+        long zeroInnNum1 = 0, zeroInnNum2 = 0;
 
-        for (int i : nums2) {
-            sum2 += i;
-            if (i == 0) {
+        long ans = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums2.length; i++){
+            sum2 += nums2[i];
+            if(nums2[i] ==0){
                 sum2++;
-                zero2++;
+                zeroInnNum2++;
             }
         }
 
-        if ((zero1 == 0 && sum2 > sum1) || (zero2 == 0 && sum1 > sum2)) {
+        for(int i = 0; i < nums1.length; i++){
+            sum1 += nums1[i];
+            if(nums1[i] ==0){
+                sum1++;
+                zeroInnNum1++;
+            }
+        }
+
+        if(sum1 < sum2 && zeroInnNum1 == 0){
+            return -1;
+        }
+        if(sum2 < sum1 && zeroInnNum2 == 0){
             return -1;
         }
 
-        return Math.max(sum1, sum2);
+         ans = Math.max(sum1 , sum2);
+
+     return ans;
+        
     }
 }
