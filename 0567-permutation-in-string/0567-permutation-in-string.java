f3c03ed9a64e17a4203d7f1185fcaf6d069a0edd
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-05-11 09:20:52 +0530

    Time: 43 ms (21.21%), Space: 45 MB (20.76%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,35 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-          int len1 = s1.length(), len2 = s2.length();
-        if (len1 > len2) return false;
+        
+        int len1 = s1.length();
+        int len2 = s2.length();
 
-        int[] count1 = new int[26]; // Frequency of characters in s1
-        int[] count2 = new int[26]; // Frequency in current window of s2
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
 
-        // Fill count1 and first window of count2
-        for (int i = 0; i < len1; i++) {
-            count1[s1.charAt(i) - 'a']++;
-            count2[s2.charAt(i) - 'a']++;
+        for(char ch : s1.toCharArray()){
+            map1.put(ch , map1.getOrDefault(ch, 0)+1);
         }
 
-        if (matches(count1, count2)) return true;
+        for(int i = 0; i < len2; i++){
+            char crrCh = s2.charAt(i);
+            map2.put(crrCh ,map2.getOrDefault(crrCh, 0) + 1);
 
-        // Slide the window over s2
-        for (int i = len1; i < len2; i++) {
-            count2[s2.charAt(i) - 'a']++; // Add new char
-            count2[s2.charAt(i - len1) - 'a']--; // Remove old char
-            if (matches(count1, count2)) return true;
+            if(i >= len1){
+                char chLeft = s2.charAt(i - len1);
+                if(map2.get(chLeft) == 1){
+                    map2.remove(chLeft);
+                }else{
+                    map2.put(chLeft , map2.get(chLeft) -1);
+                }
+            }
+
+            if(map1.equals(map2)) return true;
         }
-
         return false;
-    }
-
-    // Helper method to compare two frequency arrays
-    private boolean matches(int[] arr1, int[] arr2) {
-        for (int i = 0; i < 26; i++) {
-            if (arr1[i] != arr2[i]) return false;
-        }
-        return true;
     }
 }
